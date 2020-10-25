33be31287e5ed20c328e561415a30ba169bd93c2
James <JamesDLowe@protonmail.com>
2020-10-25 13:55:28 +0000

    initial add, subtract, multiply, divide functions created

---
+++ script.js
@@ -0,0 +1,23 @@
+function operate(x, y) {
+
+}
+
+function add(x, y) {
+    let result = x + y;
+    return result;
+}
+
+function subtract(x, y) {
+    let result = x - y;
+    return result;
+}
+
+function multiply(x, y) {
+    let result = x * y;
+    return result;
+}
+
+function divide(x, y) {
+    let result = x / y;
+    return result;
+}
